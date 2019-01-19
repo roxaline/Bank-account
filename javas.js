1dde26f1c0ff49661420b948257ea259e54027e8
roxaline <mariadelarosita078@gmail.com>
2019-01-19 02:33:53 +0200

    add som javascripts codes

---
+++ javas.js
@@ -0,0 +1,7 @@
+var x=document.getElementById('deposit').Depo.value();
+console.log(x);
+var y=document.getElementById('withdraw').Wit.value();
+console.log(y);
+function myAccount(){
+var balance = function (x, y) {return x - y};
+document.getElementById("initialB").innerHTML;
